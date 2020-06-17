0e0f3cf06688ec318ace3cd417e6ecb8194b3487
cep104 <candicelizabethpeters@gmail.com>
2020-06-16 21:28:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+main.remove()
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "${yourName} is the champion";
+document.body.appendChild(newHeader)
