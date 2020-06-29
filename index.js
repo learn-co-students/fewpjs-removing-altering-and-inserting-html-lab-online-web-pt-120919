5335802269b96d9e9b83ed51ec801b0346ca8a6d
SMakaiTakori <makait87@icloud.com>
2020-06-29 00:15:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let element = document.querySelector('main#main');
+element.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+newHeader.innerHTML = "Makai is the champion"
