2f632ee993f99b8b92566660dd2c708a86f0f388
lkorchnoy <lkorchnoy@yahoo.com>
2020-07-03 06:03:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+//element.classList.remove(main#main);
+//main.remove(main.querySelector('main')
+//let main = document.getElementById(main#main).remove(); 
+
+//newHeader = document.getElementById("h1#victory");
+
+
+document.querySelector("main#main").remove();
+
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "LUCY KORCHNOY is the champion!";
+newHeader.className = "victory";
