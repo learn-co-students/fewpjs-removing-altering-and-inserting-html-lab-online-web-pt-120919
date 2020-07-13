acd2ed3372ae5a46a5893407634ac1cba3b2083f
Matthew <mattcorralejo1@gmail.com>
2020-07-13 15:05:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+let victory = document.createElement('h1')
+victory.setAttribute("id","victory")
+document.body.appendChild(victory);
+victory.innerHTML = "YOUR-NAME is the champion"
+let newHeader = document.querySelector('h1#victory');
